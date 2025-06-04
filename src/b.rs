d7b7fa7d68328ea1ba7f6f440ffd6437410b4247
Yui <yui300127@gmail.com>
2025-06-04 17:28:38 +0300

    Make "global scope" shared between different "translation units"

--- src/b.rs
+++ src/b.rs
@@ -854,7 +854,6 @@ pub struct Compiler {
 }
 
 pub unsafe fn compile_program(l: *mut Lexer, c: *mut Compiler) -> Option<()> {
-    scope_push(&mut (*c).vars);          // begin global scope
     'def: loop {
         lexer::get_token(l)?;
         if (*l).token == Token::EOF { break 'def }
@@ -867,7 +866,7 @@ pub unsafe fn compile_program(l: *mut Lexer, c: *mut Compiler) -> Option<()> {
         let saved_point = (*l).parse_point;
         lexer::get_token(l)?;
         if (*l).token == Token::OParen { // Function definition
-            declare_var(l, &mut (*c).vars, name, name_loc, Storage::External{name});
+            declare_var(l, &mut (*c).vars, name, name_loc, Storage::External{name})?;
             scope_push(&mut (*c).vars); // begin function scope
             let mut params_count = 0;
             let saved_point = (*l).parse_point;
@@ -922,7 +921,6 @@ pub unsafe fn compile_program(l: *mut Lexer, c: *mut Compiler) -> Option<()> {
             get_and_expect_clex(l, Token::SemiColon)?;
         }
     }
-    scope_pop(&mut (*c).vars);          // end global scope
 
     Some(())
 }
@@ -1011,6 +1009,8 @@ pub unsafe fn main(mut argc: i32, mut argv: *mut*mut c_char) -> Option<()> {
     let mut c: Compiler = zeroed();
     c.target = target;
     let mut input: String_Builder = zeroed();
+
+    scope_push(&mut c.vars);          // begin global scope
     for i in 0..input_paths.count {
         let input_path = *input_paths.items.add(i);
 
@@ -1021,6 +1021,7 @@ pub unsafe fn main(mut argc: i32, mut argv: *mut*mut c_char) -> Option<()> {
 
         compile_program(&mut l, &mut c)?;
     }
+    scope_pop(&mut c.vars);          // end global scope
 
     let mut output: String_Builder = zeroed();
     let mut cmd: Cmd = zeroed();
