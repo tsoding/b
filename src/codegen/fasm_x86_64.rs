90f7c0eb16c258f924a96cd550579dafa1fe8bea
miezekatze <miezmoon64@gmail.com>
2025-06-06 14:23:23 +0200

    remove unused import

--- src/codegen/fasm_x86_64.rs
+++ src/codegen/fasm_x86_64.rs
@@ -3,7 +3,7 @@ use core::cmp;
 use crate::{Op, Binop, OpWithLocation, Arg, Func, Compiler, align_bytes};
 use crate::nob::*;
 use crate::crust::libc::*;
-use crate::{missingf, Loc};
+use crate::Loc;
 use crate::codegen::Os;
 
 pub unsafe fn load_arg_to_reg(arg: Arg, reg: *const c_char, output: *mut String_Builder) {
