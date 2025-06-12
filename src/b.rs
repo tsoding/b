22b453af5d981555c6a9c44120a74c348fc227d8
rexim <reximkut@gmail.com>
2025-06-12 20:50:29 +0700

    Fix __asm__ parsing so it produces better errors

--- src/b.rs
+++ src/b.rs
@@ -810,23 +810,27 @@ pub unsafe fn compile_statement(l: *mut Lexer, c: *mut Compiler) -> Option<()> {
             Some(())
         }
         Token::Asm => {
+            let mut args: Array<*const c_char> = zeroed();
             get_and_expect_token_but_continue(l, c, Token::OParen)?;
 
-            let mut args: Array<*const c_char> = zeroed();
-
-            while (*l).token != Token::CParen {
-                lexer::get_token(l)?;
-                match (*l).token {
-                    Token::String => {
-                        da_append(&mut args, strdup((*l).string));
+            let saved_point = (*l).parse_point;
+            lexer::get_token(l)?;
+            if (*l).token != Token::CParen {
+                (*l).parse_point = saved_point;
+                loop {
+                    get_and_expect_token(l, Token::String)?;
+                    match (*l).token {
+                        Token::String => da_append(&mut args, arena::strdup(&mut (*c).arena_names, (*l).string)),
+                        _             => unreachable!(),
                     }
-                    _ => {
-                        diagf!((*l).loc, c!("ERROR: %s only takes strings\n"), (*l).string);
-                        bump_error_count(c)?;
+
+                    get_and_expect_tokens(l, &[Token::Comma, Token::CParen])?;
+                    match (*l).token {
+                        Token::Comma  => {}
+                        Token::CParen => break,
+                        _             => unreachable!(),
                     }
                 }
-
-                get_and_expect_tokens(l, &[Token::Comma, Token::CParen])?;
             }
             get_and_expect_token_but_continue(l, c, Token::SemiColon)?;
 
