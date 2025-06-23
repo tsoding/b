c4a2fe58c4330269f1db22ffd022514ebaccfcf4
User <nevo.krien@gmail.com>
2025-06-24 01:08:08 +0300

    added sketch for autovar constant folding

--- src/opt.rs
+++ src/opt.rs
@@ -6,58 +6,41 @@ pub unsafe fn optimize(c: *mut Compiler) {
     }
 }
 
+#[derive(Clone, Copy)]
+pub struct BlockStyle{
+	no_entries:bool,
+	no_readers:bool,
+}
+
 pub unsafe fn optimize_func(c: *mut Compiler,f:*mut Func){
 	//function entry is special since no one is yet able to read/write
 	//when we see the first label that means someone may actually jump from a new context
 	//this means we can constant fold all autovar assigments here and its fine
 
-	let start = *(*f).body.items;
-	let mut block = constant_fold_block(c,f,0);
+	let mut style = BlockStyle{no_readers:false,no_entries:true};
+	let mut block = 0;
 
 	//rest is normal
 	while block < (*f).body.count {
-		block=normal_block(c,f,block);
+		block=optimize_block(c,f,block,&mut style);
 	}
 
 }
 
-pub unsafe fn normal_block(c: *mut Compiler,f: *mut Func, block: usize) -> usize{
-	let mut id = block;
-	while id < (*f).body.count {
-
-		let spot = (*f).body.items.add(id);
-		match (*spot).opcode {
-			Op::Label{ .. } | Op::Asm{..} | Op::Funcall{..}  => return id+1,
-			Op::Return{..} => {
-				///since this is returning out no one can possibly read the autovars
-				//because autovars are function local
-				//in fact this is an even more agressive type of constant folding we could do here
-
-				return constant_fold_block(c,f,block);
-
-			},
-			Op::JmpLabel{..} => {
-				return eliminate_block_end(c,f,id+1)
-
-			},
-			_ => {}
-		};
-
-		id+=1;
-	}
-
-	id
-}
-
-pub unsafe fn eliminate_block_end(c: *mut Compiler,f: *mut Func, block: usize) -> usize {
+pub unsafe fn eliminate_block_end(c: *mut Compiler,f: *mut Func, block: usize,style:*mut BlockStyle) -> usize {
 	let mut id = block;
 	
 	while id < (*f).body.count {
 
 		let spot = (*f).body.items.add(id);
 		match (*spot).opcode {
-			Op::Label{..}  => return id,
-			Op::Asm{..} => {}
+			Op::Label{..} | Op::Asm{..}  => {
+				//the asm may have a label and then fall through
+
+				(*style).no_readers = false;
+				(*style).no_entries = false;
+				return id
+			},
 			_=>{(*spot).opcode = Op::NoOp;}
 		};
 
@@ -67,18 +50,37 @@ pub unsafe fn eliminate_block_end(c: *mut Compiler,f: *mut Func, block: usize) -
 	id
 }
 
-pub unsafe fn constant_fold_block(c: *mut Compiler,f: *mut Func, block: usize) -> usize {	
+pub unsafe fn optimize_block(c: *mut Compiler,f: *mut Func, block: usize,style:*mut BlockStyle) -> usize {	
 	let mut id = block;
 	
 	while id < (*f).body.count {
 		let spot = (*f).body.items.add(id);
 
-		(*spot).opcode = eval_constant_op((*spot).opcode,(*c).target);
+		(*spot).opcode = eval_constant_op((*spot).opcode,(*c).target,*style);
 
 		match (*spot).opcode {
-			Op::Label{ .. } | Op::Asm{..} | Op::Funcall{..}  => return id+1,
-			Op::JmpLabel{..} | Op::Return{..} => {
-				return eliminate_block_end(c,f,id+1);
+			Op::Label{ .. } | Op::Asm{..} => {
+				(*style).no_entries = false;
+				(*style).no_readers = false;
+				return id+1
+			},
+			Op::Funcall{..}  => {
+				(*style).no_readers = false;
+				return id+1
+			},
+			Op::Return{..} => {
+				//since this is returning out no one can possibly read the autovars
+				//because autovars are function local
+				//in fact this is an even more agressive type of constant folding we could do here
+				if (*style).no_readers == false {
+					(*style).no_readers = true;
+					return optimize_block(c,f,block,style);
+				}
+				return eliminate_block_end(c,f,id+1,style);
+
+			},
+			Op::JmpLabel{..} => {
+				return eliminate_block_end(c,f,id+1,style);
 
 			}
 			_ => {}
@@ -90,15 +92,20 @@ pub unsafe fn constant_fold_block(c: *mut Compiler,f: *mut Func, block: usize) -
 	id
 }
 
-pub unsafe fn negate_literal(lit: u64,target: Target) -> u64 {
-    let word_size = target_word_size(target); // in bytes
-    let bits = word_size * 8;
-    let mask = if bits >= 64 { !0 } else { (1u64 << bits) - 1 };
-    (!lit).wrapping_add(1) & mask
+
+pub unsafe fn eval_constant_op(op:Op,target:Target,style:BlockStyle) -> Op{
+	if style.no_readers {
+		eval_constant_op_strong(op,target)
+	}else {
+		eval_constant_op_weak(op,target)
+	}
 }
 
+pub unsafe fn eval_constant_op_strong(op:Op,target:Target) -> Op {
+	eval_constant_op_weak(op,target)
+}
 
-pub unsafe fn eval_constant_op(op:Op,target:Target) -> Op {
+pub unsafe fn eval_constant_op_weak(op:Op,target:Target) -> Op {
 	 let mask = {
         let bits = target_word_size(target) * 8;
         if bits >= 64 { !0 } else { (1u64 << bits) - 1 }
@@ -106,7 +113,7 @@ pub unsafe fn eval_constant_op(op:Op,target:Target) -> Op {
 
 	match op {
 		Op::UnaryNot { result, arg:Arg::Literal(lit)} => Op::AutoAssign{index:result,arg:Arg::Literal((lit==0) as u64)},
-		Op::Negate { result, arg:Arg::Literal(lit)} => Op::AutoAssign{index:result,arg:Arg::Literal(negate_literal(lit,target))},
+		Op::Negate { result, arg:Arg::Literal(lit)} => Op::AutoAssign{index:result,arg:Arg::Literal((!lit).wrapping_add(1) & mask)},
 		Op::Binop { binop, index, lhs: Arg::Literal(a), rhs: Arg::Literal(b) } => {
             let val = match binop {
                 Binop::Plus          => a.wrapping_add(b),
