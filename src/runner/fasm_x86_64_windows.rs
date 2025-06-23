6b5fc564f73da363688337200736f53db6ba1173
JRedrupp <jake.redrupp@gmail.com>
2025-06-23 18:23:37 +0100

    Check wine installation before suggesting install

--- src/runner/fasm_x86_64_windows.rs
+++ src/runner/fasm_x86_64_windows.rs
@@ -17,8 +17,16 @@ pub unsafe fn run(cmd: *mut Cmd, output_path: *const c_char, run_args: *const [*
 
     if !cmd_run_sync_and_reset(cmd) { 
         if needs_wine {
-            fprintf(stderr(), c!("ERROR: Failed to run Windows executable. You may need to install Wine:\n"));
-            fprintf(stderr(), c!("  - Ubuntu/Debian: sudo apt install wine\n"));
+            // Check if wine is installed
+            cmd_append! { cmd, c!("which"), c!("wine"), }
+            let wine_not_installed = cmd_run_sync_and_reset(cmd);
+            
+            if wine_not_installed {
+                fprintf(stderr(), c!("ERROR: Failed to run Windows executable. Wine does not appear to be installed.\n"));
+                fprintf(stderr(), c!("  - Ubuntu/Debian: sudo apt install wine\n"));
+            } else {
+                fprintf(stderr(), c!("ERROR: Failed to run Windows executable.\n"));
+            }
         }
         return None; 
     }
