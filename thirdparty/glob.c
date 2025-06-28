60d4a49b8d47e01e68fefb20ed0a5408d99ad5c8
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2025-06-28 12:10:26 +0100

    btest: Use custom allocator for glob.h

--- thirdparty/glob.c
+++ thirdparty/glob.c
@@ -1,3 +1,23 @@
-// TODO: Use custom allocator to avoid malloc/free on every call?
+#include <stddef.h>
+
+char temp[1024];
+size_t temp_bump = 0;
+
+void *temp_alloc(size_t size)
+{
+    if (temp_bump + size > sizeof(temp)) return NULL;
+    void *result = temp + temp_bump;
+    temp_bump += size;
+    return result;
+}
+
+// NOTE: This is fine as "free" because glob_utf8() just frees everything at the end.
+void *temp_reset(void)
+{
+    temp_bump = 0;
+}
+
 #define GLOB_IMPLEMENTATION
+#define GLOB_MALLOC temp_alloc
+#define GLOB_FREE(...) temp_reset()
 #include "glob.h"
