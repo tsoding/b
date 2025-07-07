78c9a4e9e5c6ff3942ab8a5b48e0c705cce38bed
rexim <reximkut@gmail.com>
2025-07-07 17:23:53 +0700

    UsedFunc -> CalledName

--- src/b.rs
+++ src/b.rs
@@ -459,7 +459,7 @@ pub unsafe fn compile_primary_expression(l: *mut Lexer, c: *mut Compiler) -> Opt
 
             let var_def = find_var_deep(&mut (*c).vars, name);
             if var_def.is_null() {
-                da_append(&mut (*c).used_funcs, UsedFunc {name, loc: (*l).loc});
+                da_append(&mut (*c).called_names, CalledName {name, loc: (*l).loc});
                 Some((Arg::External(name), true))
             } else {
                 match (*var_def).storage {
@@ -993,7 +993,7 @@ pub struct Compiler {
     pub func_body: Array<OpWithLocation>,
     pub func_goto_labels: Array<GotoLabel>,
     pub func_gotos: Array<Goto>,
-    pub used_funcs: Array<UsedFunc>,
+    pub called_names: Array<CalledName>,
     pub op_label_count: usize,
     pub switch_stack: Array<Switch>,
     pub data: Array<u8>,
@@ -1009,7 +1009,7 @@ pub struct Compiler {
 }
 
 #[derive(Clone, Copy)]
-pub struct UsedFunc {
+pub struct CalledName {
     name: *const c_char,
     loc: Loc,
 }
@@ -1323,24 +1323,24 @@ pub unsafe fn main(mut argc: i32, mut argv: *mut*mut c_char) -> Option<()> {
     }
     scope_pop(&mut c.vars);          // end global scope
 
-    'used_funcs: for i in 0..c.used_funcs.count {
-        let used_global = *c.used_funcs.items.add(i);
+    'called_names: for i in 0..c.called_names.count {
+        let called_name = *c.called_names.items.add(i);
 
         for j in 0..c.funcs.count {
             let func = *c.funcs.items.add(j);
-            if strcmp(used_global.name, func.name) == 0 {
-                continue 'used_funcs;
+            if strcmp(called_name.name, func.name) == 0 {
+                continue 'called_names;
             }
         }
 
         for j in 0..c.asm_funcs.count {
             let asm_func = *c.asm_funcs.items.add(j);
-            if strcmp(used_global.name, asm_func.name) == 0 {
-                continue 'used_funcs;
+            if strcmp(called_name.name, asm_func.name) == 0 {
+                continue 'called_names;
             }
         }
 
-        diagf!(used_global.loc, c!("ERROR: could not find name `%s`\n"), used_global.name);
+        diagf!(called_name.loc, c!("ERROR: could not find name `%s`\n"), called_name.name);
         bump_error_count(&mut c);
     }
 
