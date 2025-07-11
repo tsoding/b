73017d0f4938145ad2837fdb9cc725c94b705d07
miezekatze <miezmoon64@gmail.com>
2025-07-11 22:33:13 +0200

    fix some examples, implement getchar/read

--- src/runner/mos6502.rs
+++ src/runner/mos6502.rs
@@ -2,7 +2,8 @@ use crate::nob::*;
 use crate::crust::libc::*;
 use core::ffi::*;
 
-pub const DEFAULT_LOAD_OFFSET: u16 = 0x8000;
+// load directly after stack page
+pub const DEFAULT_LOAD_OFFSET: u16 = 0x0200;
 
 #[derive(Clone, Copy)]
 pub struct Config {
@@ -11,9 +12,10 @@ pub struct Config {
 
 pub mod fake6502 {
     use core::mem::zeroed;
-    use crate::nob::*;
+    use super::*;
 
     pub static mut MEMORY: [u8; 1<<16] = unsafe { zeroed() };
+    pub static mut stdout: *mut FILE = unsafe { zeroed() };
 
     pub unsafe fn load_rom_at(rom: String_Builder, offset: u16) {
         for i in 0..rom.count {
@@ -23,11 +25,37 @@ pub mod fake6502 {
 
     #[no_mangle]
     pub unsafe extern "C" fn read6502(address: u16) -> u8 {
+        // https://www.sbprojects.net/projects/apple1/wozmon.php
+        if address == 0xD011 {
+            // KBDCR: always set, key always available
+            // TODO: this is not very accurate, real Apple-1
+            // progams would use this bit to check if key was
+            // pressed, we would have to check here if a byte
+            // is in stdin queue.
+            MEMORY[address as usize] |= 0b1000_0000;
+        } else if address == 0xD010 {
+            // TODO: if we want to be completely accurate,
+            // we would have to set bit 7 here and filter it
+            // out in B.
+            MEMORY[address as usize] = getchar() as i8 as u8;
+        }
+
         MEMORY[address as usize]
     }
 
     #[no_mangle]
-    pub unsafe extern "C" fn write6502(address: u16, value: u8) {
+    pub unsafe extern "C" fn write6502(address: u16, mut value: u8) {
+        // https://www.sbprojects.net/projects/apple1/wozmon.php
+        if address == 0xD012 { // DSP
+            // accept immediatetely => clear bit 7
+            value &= 0b0111_1111;
+            fprintf(stdout, c!("%c"), value as c_int);
+        }
+
+        if address == 0x0206 {
+            printf(c!("HERE! [0x0206 := $%x], pc := %p\n"), value as c_int, pc as c_int);
+        }
+
         MEMORY[address as usize] = value;
     }
 
@@ -46,10 +74,11 @@ pub mod fake6502 {
 
 }
 
-pub unsafe fn run_impl(output: *mut String_Builder, config: Config, stdout: *mut FILE) -> Option<()> {
+pub unsafe fn run_impl(output: *mut String_Builder, config: Config, argv_addr: u16, stdout: *mut FILE) -> Option<()> {
     fake6502::load_rom_at(*output, config.load_offset);
     fake6502::reset();
     fake6502::pc = config.load_offset;
+    fake6502::stdout = stdout;
 
     // set reset to $0000 to exit on reset
     fake6502::MEMORY[0xFFFC] = 0;
@@ -60,16 +89,13 @@ pub unsafe fn run_impl(output: *mut String_Builder, config: Config, stdout: *mut
         let opcode  = fake6502::MEMORY[fake6502::pc as usize];
         fake6502::step();
 
+        // printf(c!("pc=%p\n"), fake6502::pc as c_int);
+
         let curr_sp = fake6502::sp & 0xFF;
         if opcode == 0x48 && curr_sp > prev_sp { // PHA instruction
             log(Log_Level::ERROR, c!("Stack overflow detected"));
             log(Log_Level::ERROR, c!("SP changed from $%02X to $%02X after PHA instruction"), prev_sp as c_uint, curr_sp as c_uint);
             return None;
-        }
-
-        if fake6502::pc == 0xFFEF { // Emulating wozmon ECHO routine
-            fprintf(stdout, c!("%c"), fake6502::a as c_uint);
-            fake6502::rts();
         }
     }
     // print exit code (in Y:A)
@@ -82,7 +108,7 @@ pub unsafe fn run_impl(output: *mut String_Builder, config: Config, stdout: *mut
     Some(())
 }
 
-pub unsafe fn run(output: *mut String_Builder, config: Config, program_path: *const c_char, stdout_path: Option<*const c_char>) -> Option<()> {
+pub unsafe fn run(output: *mut String_Builder, config: Config, program_path: *const c_char, argv_addr: Option<u16>, stdout_path: Option<*const c_char>) -> Option<()> {
     (*output).count = 0;
     read_entire_file(program_path, output)?;
 
@@ -95,7 +121,7 @@ pub unsafe fn run(output: *mut String_Builder, config: Config, program_path: *co
     } else {
         stdout()
     };
-    let result = run_impl(output, config, stdout);
+    let result = run_impl(output, config, argv_addr, stdout);
     if stdout_path.is_some() {
         fclose(stdout);
     }
