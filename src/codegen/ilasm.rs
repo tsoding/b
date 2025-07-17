a8e9fe00a2176135e985bfd78fe0fd712e45cbfe
Ahmed <ahmed.walid605@yahoo.com>
2025-07-17 20:46:37 +0000

    [ilasm] update fork

--- src/codegen/ilasm.rs
+++ src/codegen/ilasm.rs
@@ -4,6 +4,7 @@ use crate::crust::libc::*;
 use crate::lexer::*;
 use crate::missingf;
 use crate::ir::*;
+use core::mem::zeroed;
 
 pub unsafe fn load_arg(loc: Loc, arg: Arg, output: *mut String_Builder, _data: *const [u8]) {
     match arg {
@@ -430,6 +431,36 @@ pub unsafe fn generate_program(
         write_entire_file(config_output_path, config as *const c_void, strlen(config))?;
     }
 
+    Some(())
+}
+
+pub unsafe fn run_program(cmd: *mut Cmd, program_path: *const c_char, run_args: *const [*const c_char], stdout_path: Option<*const c_char>, mono: bool) -> Option<()> {
+    if mono && !cfg!(target_os = "windows") {
+        cmd_append! {
+            cmd,
+            c!("mono"),
+        }
+    }
+
+    if !mono {
+        cmd_append! {
+            cmd,
+            c!("dotnet"),
+        }
+    }
+
+    cmd_append!{ cmd, program_path, }
+
+    da_append_many(cmd, run_args);
+
+    if let Some(stdout_path) = stdout_path {
+        let mut fdout = fd_open_for_write(stdout_path);
+        let mut redirect: Cmd_Redirect = zeroed();
+        redirect.fdout = &mut fdout;
+        if !cmd_run_sync_redirect_and_reset(cmd, redirect) { return None; }
+    } else {
+        if !cmd_run_sync_and_reset(cmd) { return None; }
+    }
     Some(())
 }
 
